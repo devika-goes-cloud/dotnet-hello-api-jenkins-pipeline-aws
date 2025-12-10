76a151f6d662dc90c409c2db06f898824ed9a754
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-12-11 02:01:38 +0530

    pushed new changes

---
+++ Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace hello_world_api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Use AddControllers() instead of AddMvc()
+            services.AddControllers();
+        }
+
+        // Configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
